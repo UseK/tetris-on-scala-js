a3a79af908ae35f2fed9a0567a33bfee62999062
UseK <use.k.james@gmail.com>
2017-05-03 16:21:50 +0900

    Just AdminUser, which extends User

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,30 @@
+class User(_name: String) {
+  val name = _name
+  def sayHi: Unit = {
+    println(s"Hai! $name")
+  }
+}
+
+class AdminUser(name: String, age: Int) extends User(name) {
+  override def sayHi: Unit = {
+    super.sayHi
+    println("I'm admin")
+  }
+
+}
+
+object User {
+  def apply(_name: String): User = new User(_name)
+}
+
+object AdminUser {
+  def apply(name: String, age: Int): AdminUser = new AdminUser(name, age)
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    val practice = new Practice
-    practice.expandString()
+    val tom = AdminUser("tom", 23)
+    println(tom.name)
+    tom.sayHi
   }
 }
