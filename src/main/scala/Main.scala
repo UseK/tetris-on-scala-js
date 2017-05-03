9aba28fca209edbfe0cd7cdbc9f000df4ed2adbb
UseK <use.k.james@gmail.com>
2017-05-03 16:26:53 +0900

    Constractor parameter can be used instance variable directory

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,18 @@
-class User(_name: String) {
-  val name = _name
+class User(val name: String) {
   def sayHi: Unit = {
     println(s"Hai! $name")
   }
 }
+object User {
+  def apply(name: String): User = new User(name)
+}
 
-class AdminUser(name: String, age: Int) extends User(name) {
+class AdminUser(name: String, val age: Int) extends User(name) {
   override def sayHi: Unit = {
     super.sayHi
     println("I'm admin")
   }
-
 }
-
-object User {
-  def apply(_name: String): User = new User(_name)
-}
-
 object AdminUser {
   def apply(name: String, age: Int): AdminUser = new AdminUser(name, age)
 }
@@ -24,7 +20,7 @@ object AdminUser {
 object Main {
   def main(args: Array[String]): Unit = {
     val tom = AdminUser("tom", 23)
-    println(tom.name)
+    println(s"${tom.name}(${tom.age})")
     tom.sayHi
   }
 }
