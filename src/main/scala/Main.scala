4108830fe4c6d10053ee9cfa409b0ada64cf96e0
UseK <use.k.james@gmail.com>
2017-05-03 16:08:33 +0900

    Move Aiee, Practice class to Junk.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,33 +1,3 @@
-case class Aiee(x: Int, z: String) {
-  val y: Int = x + 114514
-  def aiee: String = "called aiee"
-}
-
-class Practice {
-  def doubleLoop: IndexedSeq[String] = {
-    for (i <- 0 to 3; j <- 0 to 3; if i != j) yield s"$i-$j"
-  }
-
-  def declareAiee(x: Int): Unit = {
-    //noinspection LanguageFeature
-    implicit def String2Aiee(x: String): Aiee = Aiee(100, x)
-    val aiee = Aiee(x, "foo")
-    println(aiee.aiee)
-    println(aiee.x)
-    println(aiee.y)
-    println("Iam String".aiee)
-  }
-
-  def expandString(aiee: Aiee = Aiee(-1, "orz")): Unit = {
-    val result = aiee match {
-      case Aiee(100, z) => s"hundred: $z}"
-      case Aiee(10, z) => f"ten: ${10}%010d $z%-10s"
-      case _ => "case default..orz"
-    }
-    println(result)
-  }
-}
-
 object Main {
   def main(args: Array[String]): Unit = {
     val practice = new Practice
