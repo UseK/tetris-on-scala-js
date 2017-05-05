bee4db86e18ed597e8bf6e3a17b86dc856f1dcb4
UseK <use.k.james@gmail.com>
2017-05-05 19:30:47 +0900

    Refactoring: extract Car class, now just black rectange

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -8,25 +8,30 @@ class MyCanvas {
     this.context = canvas.getContext('2d')
     this.context.width = 480
     this.context.height = 260
-    this.x = 10
-    this.y = 10
-  }
-  drawRect() {
-    this.context.strokeRect(100, 20, 100, 60)
-    console.log(this.context.height)
-    this.context.fillRect(110, 30, 100, 60)
+    this.car = new Car(10, 10)
   }
 
   animation() {
     this.context.clearRect(0, 0, this.context.width, this.context.height)
-    this.context.fillRect(this.x, this.y, 100, 60)
-    this.x += 1
-    if (this.x > this.context.width) {
-      this.x = 0
-    }
+    this.car.animation(this.context)
     setTimeout(function() {
       this.animation()
     }.bind(this), 10)
+  }
+}
+
+class Car {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  animation(context) {
+    context.fillRect(this.x, this.y, 100, 60)
+    this.x += 1
+    if (this.x > context.width) {
+      this.x = 0
+    }
   }
 }
 (function() {
