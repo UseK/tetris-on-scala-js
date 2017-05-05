e62577f5776d70486790e19168da0b4e0514e2eb
UseK <use.k.james@gmail.com>
2017-05-05 18:29:03 +0900

    draw Rectangle

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -3,5 +3,14 @@
  */
 (function() {
     'use strict'
-    console.log("foo")
+    let stage = document.getElementById('stage')
+    if (typeof stage.getContext === 'undefined') {
+        return
+    }
+    let context = stage.getContext('2d')
+    context.width = 480
+    context.height = 260
+    context.strokeRect(100, 20, 100, 60)
+    console.log(context.height)
+    context.fillRect(110, 30, 100, 60)
 })()
