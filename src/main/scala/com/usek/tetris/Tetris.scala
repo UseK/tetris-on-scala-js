8e37e03e27d1c1532792569ef61afc56d9d9af03
UseK <use.k.james@gmail.com>
2017-06-11 08:44:31 +0900

    Refactoring

--- src/main/scala/com/usek/tetris/Tetris.scala
+++ src/main/scala/com/usek/tetris/Tetris.scala
@@ -1,19 +1,27 @@
 import com.usek.tetris._
-
 import scala.scalajs.js.JSApp
-import scala.scalajs.js.timers.setTimeout
 import scala.scalajs.js.timers.setInterval
 
 object Tetris extends JSApp{
   def main(): Unit = {
-    val r = Render("stage", width = 250, height = 500)
-    val board = new Board(20, 10)
-    var currentMino = Mino()
-    r.render(List(board, currentMino))
+    val initResult: (Render, Board, Mino) = init()
+    val render: Render = initResult._1
+    val board: Board = initResult._2
+    var currentMino: Mino = initResult._3
+
     setInterval(1000) {
-      currentMino = tick(r, currentMino, board)
+      currentMino = tick(render, currentMino, board)
     }
   }
+
+  def init() = {
+    val render = Render("stage", width = 250, height = 500)
+    val board = new Board(20, 10)
+    val currentMino = Mino()
+    render.render(List(board, currentMino))
+    (render, board, currentMino)
+  }
+
   def tick(r: Render, m: Mino, board: Board): Mino = {
     val afterMino = board.downMino(m)
     r.render(List(board, afterMino))
