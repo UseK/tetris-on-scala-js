dd390a97ea1db65852c8ba9bff69307b5920174d
UseK <use.k.james@gmail.com>
2017-07-03 19:37:50 +0900

    Bugfix: when downed, shape become default

--- src/main/scala/com/usek/tetris/Mino.scala
+++ src/main/scala/com/usek/tetris/Mino.scala
@@ -6,11 +6,8 @@ package com.usek.tetris
 case class Position(val x: Int, val y:Int)
 
 
-class Mino(val position: Position=Position(0, 0),
-           val shape: List[(Int, Int)]=List((0, 0), (0, 1), (1, 1), (2, 1))
-          ) extends Renderable {
-
-//class Mino(val position: Position=Position(0, 0)) extends Renderable {
+class Mino(val position: Position,
+           val shape: List[(Int, Int)]) extends Renderable {
   def rotated(): Mino = {
     val rotatedShape = this.shape.map((item) =>
       (-item._2, item._1)
@@ -19,14 +16,14 @@ class Mino(val position: Position=Position(0, 0),
   }
 
 
+  def moved(x: Int, y:Int): Mino = {
+    Mino(Position(position.x + x, position.y + y), shape)
+  }
+
   def eachPositions(op: (Int, Int) => Unit) {
     shape.foreach((item) =>
       op(item._1 + position.x, item._2 + position.y)
     )
-  }
-
-  def moved(x: Int, y:Int): Mino = {
-    Mino(Position(position.x + x, position.y + y))
   }
 
   override def render(r: Render): Unit = {
@@ -35,7 +32,9 @@ class Mino(val position: Position=Position(0, 0),
 }
 
 object Mino {
-  def apply(position: Position=Position(0, 0)): Mino = {
-    new Mino(position)
+  def apply(position: Position=Position(0, 0),
+            shape: List[(Int, Int)]=List((0, 0), (0, 1), (1, 1), (2, 1))
+           ): Mino = {
+    new Mino(position, shape)
   }
 }
--- src/test/scala/MinoSuite.scala
+++ src/test/scala/MinoSuite.scala
@@ -21,4 +21,13 @@ class MinoSuite extends  FunSuite {
 
     assert(expected == rotatedMino.shape)
   }
+
+  test("Mino shoud be still rotated when mino is downed") {
+    val mino = Mino()
+    val rotatedMino = mino.rotated()
+    val downedMino = rotatedMino.moved(0, 1)
+
+    val expected = List((0,0), (-1,0), (-1,1), (-1,2))
+    assert(expected == downedMino.shape)
+  }
 }
